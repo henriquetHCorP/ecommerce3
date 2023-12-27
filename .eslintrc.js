b0f83dfa354ba22bf1129b87359a52d4f95bcafb
Henriquet KAPEMA <henriquetkapema@gmail.com>
2023-12-27 15:25:07 +0100

    ignore typescript(only for now!)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier':['error', { endOfLine: 'auto'}], 
+    // 'prettier/prettier':['error', { endOfLine: 'auto'}], 
     'no-console':'off',  
+    'prettier/prettier': ["error", { endOfLine: "off" }]
   }
 }
